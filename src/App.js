7b4e597349337c671801398dac249d2107939a1f
ashoksinghbalot <as7416063@gmail.com>
2023-05-31 14:34:57 +0530

    frist commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from 'react';
+import Layout from './components/layout';
+import Footer from './components/footer';
 
 function App() {
+
+  const [test, setTest ] = useState(true)
+
+  const incrementCounter = () => {
+    setTimeout(() => {
+      setTest(false)
+    }, 20000);
+  }
+
+  useEffect(() => {
+    incrementCounter()
+  },[test])
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+    {/* // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header> */}
+
+      <Layout mmyAttribute = "test" />
+      {test && <Footer />}
     </div>
   );
 }
---
+++ src/components/layout.js
@@ -0,0 +1,10 @@
+const Layout = (props) => {
+    return (
+        <div id = "header">
+            <h1>my header {props.mmyAttribute}</h1>
+
+        </div>
+    )
+}
+
+export default Layout
